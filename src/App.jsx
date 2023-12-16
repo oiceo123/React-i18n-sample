34a79ce999ba93e33e04b1e1952b0fbd22319072
oiceo123 <iceome44@gmail.com>
2023-12-17 02:49:48 +0700

    🔥 remove code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
 import { useTranslation, Trans } from "react-i18next";
 
 function App() {
   const { t, i18n } = useTranslation();
-  const [count, setCount] = useState(0);
 
   const lngs = {
     en: { nativeName: "English" },
@@ -16,15 +11,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div>
         {Object.keys(lngs).map((lng) => (
           <button
@@ -37,18 +23,10 @@ function App() {
           </button>
         ))}
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <Trans i18nKey={"description"}>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </Trans>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        <Trans i18nKey={"description"}>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </Trans>
       </p>
       <p>{t("learn")}</p>
     </>
